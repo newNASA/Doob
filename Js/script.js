922791b22578233037eac2456b95e75c410ef179
newNASA <azizbekgulomovweb@gmail>
2023-08-28 20:52:21 +0500

    add js for lang

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,17 @@
-window.addEventListener("scroll", function () {
-      let header = document.querySelector(".navbar");
-      header.classList.toggle("sticky", window.scrollY > 0)
-});
+let langs = document.querySelector('.langs');
+let lang = document.querySelector('.lang');
+let langClickedCount = 0;
+
+function langClicked() {
+      langClickedCount++;
+
+      if (langClickedCount % 3 === 1) {
+            langs.style.display = 'block';
+      } else if (langClickedCount % 3 === 2) {
+            langs.style.display = 'none';
+      } else {
+            langs.style.display = 'block';
+      }
+}
+
+lang.addEventListener('click', langClicked);
